7546d3ba22456ef225dac0dbe0be1d205217a81c
COMPU-S023\daw1 <mcarlosi01@educantabria.es>
2024-10-21 10:43:39 +0200

    SEGUNDO OBJETIVO

---
+++ Objetivos/src/Objetivo2.java
@@ -0,0 +1,14 @@
+public class Objetivo2 {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(system.in);
+
+        int Objetivo2 = 0;
+
+        System.out.println("Indica el numero que quieres mostar pot pantalla");
+            
+        Objetivo2 = scanner.nextInt();
+
+        System.out.println(Objetivo2);
+    }
+}
